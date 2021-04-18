14d802e84dfa66f25801e2658ef3af9ae022f0c2
malwarehenri <marazmemu@hotmail.com>
2021-04-18 13:50:35 +0300

    converting boolean added

--- main_works/lesson10.go
+++ main_works/lesson10.go
@@ -18,13 +18,29 @@ func main() {
 		h          = int16(bh)
 		// c = "10" - 1 quickcheck 10.1 , with an error:
 		//The range keyword can decode a UTF-8 encoded string into runes
-		countdown = 1
-		str       = "hmm there is " + strconv.Itoa(countdown) + " string in here." // strconv package's Atoi function(ascii to int)
-		a         = int_to_string(45)
+		countdown  = 1
+		str        = "hmm there is " + strconv.Itoa(countdown) + " string in here." // strconv package's Atoi function(ascii to int)
+		a          = int_to_string(45)
+		ready      = false
+		readytext  = fmt.Sprintf("%v", ready)
+		readyOrnot string
 	)
 	fmt.Println(h)
 	fmt.Println(str)
-	fmt.Println(fmt.Sprintf("There is %v more string in here.", countdown)) // another way to convert a number to a string with use Sprintf
+	fmt.Println(fmt.Sprintf("There is %v more string in here.",
+		countdown)) // another way to convert a number to a string with use Sprintf
+
 	fmt.Printf("a : %v\n", a)
-	fmt.Printf(" type of a : %T", a) // prints type of a : string.
+	fmt.Printf("type of a : %T\n", a) // prints type of a : string.
+
+	fmt.Println(readytext)
+
+	if ready {
+		readyOrnot = "yes"
+	} else {
+		readyOrnot = "no"
+	}
+
+	fmt.Println("Ready or not : ", readyOrnot)
+
 }
