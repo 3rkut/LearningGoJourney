7324fbd496b97b6db628eca9e78d963d1210c57f
malwarehenri <marazmemu@hotmail.com>
2021-04-18 13:38:50 +0300

    int to string func added

--- main_works/lesson10.go
+++ main_works/lesson10.go
@@ -5,6 +5,10 @@ import (
 	"strconv"
 )
 
+func int_to_string(x int) string { // convert int to string with this.
+	return strconv.Itoa(x)
+}
+
 func main() {
 
 	downit := "lets use plus operator " + "2" + " times."
@@ -15,10 +19,12 @@ func main() {
 		// c = "10" - 1 quickcheck 10.1 , with an error:
 		//The range keyword can decode a UTF-8 encoded string into runes
 		countdown = 1
-		str       = "hmm there is " + strconv.Itoa(countdown) + " string in here."
+		str       = "hmm there is " + strconv.Itoa(countdown) + " string in here." // strconv package's Atoi function(ascii to int)
+		a         = int_to_string(45)
 	)
 	fmt.Println(h)
 	fmt.Println(str)
-	fmt.Println(fmt.Sprintf("There is %v more string in here.", countdown))
-
+	fmt.Println(fmt.Sprintf("There is %v more string in here.", countdown)) // another way to convert a number to a string with use Sprintf
+	fmt.Printf("a : %v\n", a)
+	fmt.Printf(" type of a : %T", a) // prints type of a : string.
 }
