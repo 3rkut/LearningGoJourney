96cca521e48898780d2c69bb0bfd476b296f40c2
malwarehenri <marazmemu@hotmail.com>
2021-04-22 18:16:06 +0300

    listing 13.2 code

--- main_works/lesson13.go
+++ main_works/lesson13.go
@@ -8,4 +8,12 @@ func main() {
 
 	var temperature celcius = 20
 	fmt.Println(temperature)
+
+	const degrees = 20
+	temperature = degrees
+	temperature += 10
+
+	var warmUp float64 = 10
+	temperature += warmUp // not same type.
+
 }
