06d99ffffe08ffcd002297e0cdfb5930ccf41d22
malwarehenri <marazmemu@hotmail.com>
2021-04-22 19:07:43 +0300

    method codes added

--- main_works/lesson13.go
+++ main_works/lesson13.go
@@ -2,18 +2,30 @@ package main
 
 import "fmt"
 
+type Ogrenci struct {
+	yas    int
+	isim   string
+	notlar []int
+}
+
+func (x Ogrenci) getAge() int {
+	return x.yas
+
+}
+
+func (y *Ogrenci) setAge(yas int) {
+	y.yas = yas
+}
+
 func main() {
 
-	type celcius float64
+	ogrenci1 := Ogrenci{isim: "Case", yas: 20, notlar: []int{70, 80}}
+	fmt.Println(ogrenci1.notlar)
+	fmt.Println(ogrenci1.getAge())
 
-	var temperature celcius = 20
-	fmt.Println(temperature)
-
-	const degrees = 20
-	temperature = degrees
-	temperature += 10
-
-	var warmUp float64 = 10
-	temperature += warmUp // not same type.
+	ogrenci2 := Ogrenci{isim: "Johny", yas: 18, notlar: []int{55, 60}}
+	fmt.Println(ogrenci2)
+	ogrenci2.setAge(19)
+	fmt.Println(ogrenci2)
 
 }
