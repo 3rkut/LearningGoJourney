8e2c70456a41a9749069cfa64232728f6bcb6de3
malwarehenri <marazmemu@hotmail.com>
2021-04-25 20:16:31 -0400

    added for range code example

--- main_works/lesson13.1.go
+++ main_works/lesson13.1.go
@@ -18,7 +18,14 @@ func main() {
 	var (
 		a     = myfunc1(8, 15)
 		b, b2 = myfunc2(4, 5)
+		c     = []int{1, 3, 4, 5} // example of defining an array.
 	)
+
 	fmt.Printf("result: %d\n", a) // 8 - 15 - (17)
 	fmt.Println("sum result:", b, "multiply result: ", b2)
+	fmt.Println(c)
+
+	for index1, value2 := range c { // another example of array with for range.
+		fmt.Println("index: ", index1, "value: ", value2)
+	}
 }
