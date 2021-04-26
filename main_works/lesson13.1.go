a2e9aaf928a459b21342f254ab1df02d6ef1ed70
malwarehenri <marazmemu@hotmail.com>
2021-04-25 20:04:05 -0400

    added hypot function

--- main_works/lesson13.1.go
+++ main_works/lesson13.1.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
+func myfunc1(a, b float64) float64 { // pisagor :)
+	return math.Sqrt(a*a + b*b)
+}
 func main() {
+
+	a := myfunc1(8, 15)
+	fmt.Printf("result: %d", a)
 
 }
