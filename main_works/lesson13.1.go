d62d4e932a5d00faeb1088d19fbc81449ee8fc2d
malwarehenri <marazmemu@hotmail.com>
2021-04-25 20:10:21 -0400

    added return 2 value code

--- main_works/lesson13.1.go
+++ main_works/lesson13.1.go
@@ -8,9 +8,17 @@ import (
 func myfunc1(a, b float64) float64 { // pisagor :)
 	return math.Sqrt(a*a + b*b)
 }
+
+func myfunc2(x, y int) (int, int) {
+	return x + y, x * y
+}
+
 func main() {
 
-	a := myfunc1(8, 15)
-	fmt.Printf("result: %d", a)
-
+	var (
+		a     = myfunc1(8, 15)
+		b, b2 = myfunc2(4, 5)
+	)
+	fmt.Printf("result: %d\n", a) // 8 - 15 - (17)
+	fmt.Println("sum result:", b, "multiply result: ", b2)
 }
