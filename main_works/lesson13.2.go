fc4a59c01ec73c44c30a75a89211835a0a0775f5
malwarehenri <marazmemu@hotmail.com>
2021-04-25 20:35:01 -0400

    pointers with function

--- main_works/lesson13.2.go
+++ main_works/lesson13.2.go
@@ -7,7 +7,20 @@ func f() *int {
 	return &v
 }
 
+func incr(p *int) int { // paramater is important in this example.
+	*p++ // v += 1 at this point.
+	return *p
+}
+
 func main() {
 
-	fmt.Println(f() == f()) // prints false.
+	fmt.Println(f() == f()) // Prints false.
+
+	v := 1                            // v = 1.
+	fmt.Println("address of v: ", &v) // check v's address.
+	incr(&v)                          // parameter > address of v.
+	// now v is 2.
+	fmt.Println("new v value: ", v)   // check v's value.
+	fmt.Println(incr(&v))             // now v is 3.
+	fmt.Println("address of v: ", &v) // still same address.
 }
