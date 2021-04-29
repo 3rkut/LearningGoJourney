94a3ee1d3d6335cc4e6eab53a0b413031831ad3c
malwarehenri <marazmemu@hotmail.com>
2021-04-30 02:12:12 +0300

    created

--- data_structures_algorithms/Chapter1/chapter1.0.go
+++ data_structures_algorithms/Chapter1/chapter1.0.go
@@ -8,18 +8,30 @@ import (
 	"fmt"
 )
 
+func powerSeries(x int) (int, int) {
+	return x * x, x * x * x
+}
+
+func powerSeries2(x int) (square int, cube int) {
+	square = x * x
+	cube = square * x
+	return
+}
 func main() {
 
-	var intList list.List
+	var (
+		intList        list.List
+		square, cube   = powerSeries(4)
+		square2, cube2 = powerSeries2(5)
+	)
 	intList.PushBack(21)
 	intList.PushBack(53)
 
-	for element:= intList.Front(); element!= nil; element = element.Next()
-	{
+	for element := intList.Front(); element != nil; element = element.Next() {
 		fmt.Println(element.Value.(int))
 	}
+
+	fmt.Println("square: ", square, "cube: ", cube)
+	fmt.Println("squar2: ", square2, "cube2: ", cube2)
+
 }
-
-
-
-
