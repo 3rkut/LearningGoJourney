2d9a770a990eea3bfa4545b04be666ae2acaa0ad
malwarehenri <marazmemu@hotmail.com>
2021-04-30 02:25:13 +0300

    code added

--- data_structures_algorithms/Chapter1/chapter1.0.go
+++ data_structures_algorithms/Chapter1/chapter1.0.go
@@ -4,12 +4,34 @@
 package main
 
 import (
+	"container/heap"
 	"container/list"
 	"fmt"
 )
 
+type IntegerHeap []int
+
+func (iheap IntegerHeap) Len() int { return len(iheap) }
+
+func (iheap IntegerHeap) Less(i, j int) bool { return iheap[i] < iheap[j] }
+
+func (iheap IntegerHeap) Swap(i, j int) { iheap[i], iheap[j] = iheap[j], iheap[i] }
+
+func (iheap *IntegerHeap) Push(heapintf interface{}) {
+	*iheap = append(*iheap, heaprintf.(int))
+}
+
 func powerSeries(x int) (int, int) {
 	return x * x, x * x * x
+}
+
+func (iheap *IntegerHeap) Pop() interface{} {
+	var n, x1 int
+	var previous IntegerHeap = *iheap
+	n = len(previous)
+	x1 = previous[n-1]
+	*iheap = previous[0 : n-1]
+	return x1
 }
 
 func powerSeries2(x int) (square int, cube int) {
@@ -34,4 +56,11 @@ func main() {
 	fmt.Println("square: ", square, "cube: ", cube)
 	fmt.Println("squar2: ", square2, "cube2: ", cube2)
 
+	var intHeap *IntegerHeap = &IntegerHeap{1, 4, 5}
+	heap.Init(intHeap)
+	heap.Push(intHeap, 2)
+	fmt.Printf("minimum: %d\n", (*intHeap)[0])
+	for intHeap.Len() > 0 {
+		fmt.Printf("%d \n", heap.Pop(intHeap))
+	}
 }
