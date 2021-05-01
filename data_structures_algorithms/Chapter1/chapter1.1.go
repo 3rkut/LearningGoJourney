29eace493b0b21b12f1b6817639b7c857a22878b
malwarehenri <marazmemu@hotmail.com>
2021-05-01 15:39:40 +0300

    created

---
+++ data_structures_algorithms/Chapter1/chapter1.1.go
@@ -0,0 +1,21 @@
+// chapter1.1.go
+// malwarehenri
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var p *int
+	a := 10
+	p = &a
+
+	fmt.Println("a: ", a)
+	fmt.Println("&a: ", &a)
+	fmt.Println("p: ", p)
+	fmt.Println("&p: ", &p)
+	fmt.Println("*p: ", *p)
+}
