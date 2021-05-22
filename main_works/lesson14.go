699e4ff197a1d6f83a0b41f55026c18d6ab6afe3
malwarehenri <marazmemu@hotmail.com>
2021-05-22 19:41:44 +0300

    defining slice line

--- main_works/lesson14.go
+++ main_works/lesson14.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	hello := []string{sayHello(""), sayHello("joseph")} // slice.
+	fmt.Println(hello)
+}
 
-
+func sayHello(name string) string {
+	return "Like a diamond..." + name
 }
