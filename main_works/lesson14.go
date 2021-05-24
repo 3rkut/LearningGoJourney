6d777e16efac8d729a156a6bdbed3c38559e67dd
malwarehenri <marazmemu@hotmail.com>
2021-05-24 13:25:17 +0300

    deleted for loop

--- main_works/lesson14.go
+++ main_works/lesson14.go
@@ -3,12 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	hello := []string{sayHello(""), sayHello("joseph")} // slice.
-	hello = append(hello, " 50 shades of grey ")
 
-	for i, data := range hello {
-		fmt.Println(i, data)
-	}
 	hello2 := heck{"Hello Dear Teacher.", sayHello("XD")}
 	// run with this command : "go run lesson14.go lesson14.1go"
 	fmt.Println(hello2)
