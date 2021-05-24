957ed8f76e9480ddffae1908cd9ed29cf59375e7
malwarehenri <marazmemu@hotmail.com>
2021-05-24 13:26:30 +0300

    added print receiver

--- main_works/lesson14.go
+++ main_works/lesson14.go
@@ -7,6 +7,7 @@ func main() {
 	hello2 := heck{"Hello Dear Teacher.", sayHello("XD")}
 	// run with this command : "go run lesson14.go lesson14.1go"
 	fmt.Println(hello2)
+	hello2.print() // call from the lesson14.1.go
 }
 
 func sayHello(name string) string {
