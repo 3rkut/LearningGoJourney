bed0166f3d9df58ba8fb25ab10763cba46db7dd3
malwarehenri <marazmemu@hotmail.com>
2021-05-24 13:22:40 +0300

    call type from lesson14.1.go

--- main_works/lesson14.go
+++ main_works/lesson14.go
@@ -9,6 +9,9 @@ func main() {
 	for i, data := range hello {
 		fmt.Println(i, data)
 	}
+	hello2 := heck{"Hello Dear Teacher.", sayHello("XD")}
+	// run with this command : "go run lesson14.go lesson14.1go"
+	fmt.Println(hello2)
 }
 
 func sayHello(name string) string {
