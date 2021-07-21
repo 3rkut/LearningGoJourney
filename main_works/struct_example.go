0bd7d3624ce6b4779b42f9d4499f9b09d2a27129
3rkut <marazmemu@hotmail.com>
2021-07-22 00:16:15 +0300

    error handling

--- main_works/struct_example.go
+++ main_works/struct_example.go
@@ -1,31 +1,20 @@
 package main
 
-import "fmt"
-
-type exampleStruct struct {
-	name string
-	id   int
-	exampleStruct2
-}
-
-type exampleStruct2 struct {
-	surname string
-}
+import (
+	"errors"
+	"fmt"
+	"log"
+)
 
 func main() {
-	person1 := exampleStruct{"Lucifer", 1, exampleStruct2{"Morningstar"}}
-	fmt.Println(person1.Hellothere())
-	person1.id = 10
-	fmt.Printf("person1 id: %v\n", person1.id)
-	person1.UpdateID(20)
-	fmt.Printf("person1 id: %v\n", person1.id)
-	fmt.Println(person1.exampleStruct2.surname)
+	test, err := checkError()
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(test)
 }
 
-func (es exampleStruct) Hellothere() string {
-	return fmt.Sprintf("welcome to the club, %s\n", es.name)
-}
+func checkError() (string, error) {
+	return "", errors.New("Error detected!")
 
-func (es *exampleStruct) UpdateID(id int) {
-	es.id = id
 }
