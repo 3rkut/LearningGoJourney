95543d0a804f4396ab39fe38ffee5b7d3a41d4b6
3rkut <marazmemu@hotmail.com>
2021-07-21 23:46:22 +0300

    define another struct in main struct

--- main_works/struct_example.go
+++ main_works/struct_example.go
@@ -5,13 +5,27 @@ import "fmt"
 type exampleStruct struct {
 	name string
 	id   int
+	exampleStruct2
+}
+
+type exampleStruct2 struct {
+	surname string
 }
 
 func main() {
-	person1 := exampleStruct{"Lucifer", 1}
+	person1 := exampleStruct{"Lucifer", 1, exampleStruct2{"Morningstar"}}
 	fmt.Println(person1.Hellothere())
+	person1.id = 10
+	fmt.Printf("person1 id: %v\n", person1.id)
+	person1.UpdateID(20)
+	fmt.Printf("person1 id: %v\n", person1.id)
+	fmt.Println(person1.exampleStruct2.surname)
 }
 
 func (es exampleStruct) Hellothere() string {
 	return fmt.Sprintf("welcome to the club, %s\n", es.name)
+}
+
+func (es *exampleStruct) UpdateID(id int) {
+	es.id = id
 }
