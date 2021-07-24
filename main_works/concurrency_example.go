ebabf47f232d49c5e16fa8210a6794c3b3d9b3a3
3rkut <marazmemu@hotmail.com>
2021-07-24 17:58:01 +0300

    sync-wg usage with anonymous func for goroutine.

--- main_works/concurrency_example.go
+++ main_works/concurrency_example.go
@@ -2,23 +2,22 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func main() {
-	go countI()
-	go countT()
-	time.Sleep(time.Second)
+func main() { // func main is first Goroutine.
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		countI()
+		wg.Done()
+	}()
+	wg.Wait()
 }
 
 func countI() {
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
-	}
-}
-
-func countT() {
-	for t := 0; t < 10; t++ {
-		fmt.Println(t)
 	}
 }
